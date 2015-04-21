1f8265bcae4f7aaa1108fb34c8710e3876e370f1
arturo castro <arturo@openframeworks.cc>
2015-04-21 10:24:29 +0200

    first commit

---
+++ build.rs
@@ -0,0 +1,30 @@
+extern crate pkg_config;
+
+use std::process::Command;
+use std::env;
+use std::fs;
+use std::path::Path;
+
+#[cfg(unix)]
+fn main() {
+    match pkg_config::find_library("freetype2") {
+        Ok(_) => return,
+        Err(_) => {
+			let freetype_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+			let freetype_native_dir = Path::new(&freetype_dir).join("freetype2");
+			Command::new("./autogen.sh")
+				.current_dir(&freetype_native_dir)
+				.status().unwrap();
+			Command::new("./configure")
+				.current_dir(&freetype_native_dir)
+				.status().unwrap();
+			Command::new("make")
+				.current_dir(&freetype_native_dir)
+				.status().unwrap();
+			let out_dir = env::var("OUT_DIR").unwrap();
+			let dest_path = Path::new(&out_dir).join("libfreetype.a");
+			fs::copy(freetype_native_dir.join("objs/.libs/libfreetype.a"),dest_path).unwrap();
+			println!("cargo:rustc-flags= -L native={}",out_dir);
+		}
+    }
+}
