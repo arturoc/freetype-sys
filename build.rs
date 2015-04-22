2b3a67881913f2251d67bde4e6f7dae0a6b72a22
arturo castro <arturo@openframeworks.cc>
2015-04-22 16:59:59 +0200

    download freetype from tars instead of git

--- build.rs
+++ build.rs
@@ -1,8 +1,10 @@
+#![feature(path_ext)]
 extern crate pkg_config;
+extern crate hyper;
 
 use std::process::Command;
 use std::env;
-use std::fs;
+use std::fs::{self,PathExt};
 use std::path::Path;
 
 #[cfg(unix)]
@@ -11,10 +13,13 @@ fn main() {
         Ok(_) => return,
         Err(_) => {
 			let freetype_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-			let freetype_native_dir = Path::new(&freetype_dir).join("freetype2");
-			Command::new("./autogen.sh")
-				.current_dir(&freetype_native_dir)
-				.status().unwrap();
+			let freetype_native_dir = Path::new(&freetype_dir).join("freetype-2.5.5");
+            if !freetype_native_dir.exists() {
+            	hyper::Client::new()
+            		.get("http://download.savannah.gnu.org/releases/freetype/freetype-doc-2.5.5.tar.gz")
+            		.send()
+            		.unwrap();
+            }	
 			Command::new("./configure")
 				.current_dir(&freetype_native_dir)
 				.status().unwrap();
