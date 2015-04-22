85d4dbc222f17a47f63103f40536ee0010e49c3b
arturo <arturo@openframeworks.cc>
2015-04-22 17:28:53 +0200

    add freetype to repo, everything else seems to fail

--- build.rs
+++ build.rs
@@ -1,9 +1,8 @@
-#![feature(path_ext)]
 extern crate pkg_config;
 
 use std::process::Command;
 use std::env;
-use std::fs::{self,PathExt};
+use std::fs;
 use std::path::Path;
 
 #[cfg(unix)]
@@ -13,19 +12,6 @@ fn main() {
         Err(_) => {
 			let freetype_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 			let freetype_native_dir = Path::new(&freetype_dir).join("freetype-2.5.5");
-            if !freetype_native_dir.exists() {
-				Command::new("curl")
-					.current_dir(&freetype_dir)
-					.arg("-L")
-					.arg("http://download.savannah.gnu.org/releases/freetype/freetype-2.5.5.tar.gz")
-					.arg("-O")
-					.status().unwrap();
-				Command::new("tar")
-					.current_dir(&freetype_dir)
-					.arg("xzf")
-					.arg("freetype-2.5.5.tar.gz")
-					.status().unwrap();
-            }	
 			Command::new("./configure")
 				.current_dir(&freetype_native_dir)
 				.status().unwrap();
